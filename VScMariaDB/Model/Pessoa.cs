92faf4a50e7c761c4c789cd5ef078874a1477c10
AndreAS200320 <129907649+AndreAS200320@users.noreply.github.com>
2024-02-07 21:58:04 -0300

    ult

---
+++ VScMariaDB/Model/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VScMariaDB.Model
+{
+    public class Pessoa
+    {
+        public int Id{ get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(15)]
+        public string? Numero { get; set; }
+
+        public DateOnly DataNascimento { get; set; }
+    }
+}
---
+++ VScMariaDB/Model/_DBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace VScMariaDB.Model
+{
+
+    
+
+    public class _DbContext : DbContext
+        {
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)      
+                {      
+                    IConfigurationRoot Configuration = new ConfigurationBuilder()      
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)      
+                    .AddJsonFile("appsettings.json")      
+                    .Build();
+                
+                    var ConnectionString = Configuration.GetConnectionString("MariaDB");      
+                    OptionsBuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));      
+                }   
+        }
+        
+}
